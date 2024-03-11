38e2cabbfb2e98e90797698d307ce6d12c1330cd
pityke42 <ballllaistvan@gmail.com>
2024-03-11 22:36:20 +0100

    generics

---
+++ src/main/java/org/example/generics/challenge2/util/QueryList.java
@@ -0,0 +1,31 @@
+package org.example.generics.challenge2.util;
+
+import org.example.generics.extras.model.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QueryList <T extends Student & QueryItem>{
+    private List<T> items;
+    public QueryList(List<T> items){
+        this.items = items;
+    }
+    public List<T> getMatches(String field, String value){
+        List<T> matches = new ArrayList<>();
+        for(var item : items){
+            if(item.matchFieldValue(field, value)){
+                matches.add(item);
+            }
+        }
+        return matches;
+    }
+    public static <S extends QueryItem>List<S> getMatches(List<S> items, String field, String value){
+        List<S> matches = new ArrayList<>();
+        for(var item : items){
+            if(item.matchFieldValue(field, value)){
+                matches.add(item);
+            }
+        }
+        return matches;
+    }
+}
---
+++ src/main/java/org/example/generics/extras/model/Student.java
@@ -0,0 +1,41 @@
+package org.example.generics.extras.model;
+
+import org.example.generics.extras.util.QueryItem;
+
+import java.util.Random;
+
+public class Student implements QueryItem {
+    private String name;
+    private String course;
+    private int yearStarted;
+    protected static Random random = new Random();
+    private static String[] firstName = {"Ann", "Bill", "Cathy", "John", "Tim"};
+    private static String[] courses = {"C++", "Java", "Js", "Python"};
+
+    public Student() {
+        int lastNameIndex = random.nextInt(65, 91);
+        name = firstName[random.nextInt(5)] + " " + (char) lastNameIndex;
+        course = courses[random.nextInt(4)];
+        yearStarted = random.nextInt(2018, 2024);
+    }
+
+    @Override
+    public String toString() {
+        return "%-15s %-15s %d".formatted(name, course, yearStarted);
+    }
+
+    public int getYearStarted() {
+        return yearStarted;
+    }
+
+    @Override
+    public boolean matchFieldValue(String fieldName, String value) {
+        String fName = fieldName.toUpperCase();
+        return switch(fName){
+            case"NAME" -> name.equalsIgnoreCase(value);
+            case "COURSE" -> course.equalsIgnoreCase(value);
+            case "YEARSTARTED" -> yearStarted == (Integer.parseInt(value));
+            default -> false;
+        };
+    }
+}
